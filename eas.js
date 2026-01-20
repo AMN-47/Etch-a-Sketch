0649d8961d1d5b599fbd085fd8a9ef53b2ae696e
AMN-47 <akhilnagar57@gmail.com>
2026-01-20 20:49:15 +0200

    fixed template and column row to use correct symbol

--- eas.js
+++ eas.js
@@ -1,11 +1,11 @@
 
 
-function populateBoard(size){
+function populateBoard(size) {
     let board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
     squares.forEach((div) => div.remove());
-    board.style.gridTemplateColumns = 'repeat(${size}, 1fr)';
-    board.style.gridTemplateRows = 'repeat(${size}, 1fr)'; 
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
     let amount = size*size;
     for (let i = 0; i < amount; i++) {
