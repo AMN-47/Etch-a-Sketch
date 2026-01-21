5ce571566bce422b5a69d160aab5880eaaa58768
AMN-47 <akhilnagar57@gmail.com>
2026-01-21 14:49:29 +0200

    added function to reset board on reset button

--- eas.js
+++ eas.js
@@ -36,4 +36,10 @@ function colorSquare() {
 
 function changeColor(choice) {
     color = choice;
+} 
+
+function resetBoard () {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
 }
